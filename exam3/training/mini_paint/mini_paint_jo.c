3fb3d22286e419a963d558a620247df6b522a3c9
Joann ACHER <jacher@e2r7p4.42.fr>
2021-03-24 11:56:17 +0100

    mini paint ok

--- exam3/training/mini_paint/mini_paint_jo.c
+++ exam3/training/mini_paint/mini_paint_jo.c
@@ -7,16 +7,16 @@ typedef struct s_global
 {
 	int width;
 	int height; 
-	char *c;
+	char c;
 }			t_global;
 
 typedef struct s_operation
 {
-	char *letter;
+	char letter;
 	float x;
 	float y;
 	float r;
-	char *c;
+	char c;
 }			t_op;
 
 int ft_strlen(char *s)
@@ -45,13 +45,18 @@ int in_circle(float x1, float y1, t_op *op)
 {
 	float distance;
 
-	distance = sqrtf((x1 - op->x) * (x1 - op->x) + (y1 - op->y) * (y1 -op->y));
+	distance = sqrtf((x1 - op->x) * (x1 - op->x) + (y1 - op->y) * (y1 - op->y));
 	if ( distance <= op->r)
 	{
-		if (distance < op->r)
+		if (op->r - distance < 1.0) // pour la bordure
+		{
+		//	printf("--2--\n");
 			return(2);
+		}
+	//	printf("--1--\n");
 		return (1);
 	}
+//	printf("--0--\n");
 	return (0);
 }
 
@@ -61,14 +66,11 @@ char *read_first_line(FILE *file, t_global *g)
 	int i;
 	char *str;
 
-	res = fscanf(file, "%d %d %s\n", &(g->width), &(g->height), g->c);
-	printf("width = %d | height = %d | letter = %c\n", g->width, g->height, g->c[0]);
+	res = fscanf(file, "%d %d %c\n", &(g->width), &(g->height), &(g->c));
 	if (res != 3)
 		return (NULL);
-	printf("width = %d | height = %d | letter = %c\n", g->width, g->height, g->c[0]);
 	if (g->width <= 0 || g->width > 300 || g->height <= 0|| g->height > 300)
 		return (NULL);
-	printf("width = %d | height = %d | letter = %c\n", g->width, g->height, g->c[0]);
 	str = malloc(sizeof(char) * ((g->width * g->height) + g->height + 1));
 	if (str == NULL)
 		return (NULL);
@@ -78,11 +80,10 @@ char *read_first_line(FILE *file, t_global *g)
 		if ((i + 1) % (g->width + 1) == 0)
 			str[i] = '\n';
 		else
-			str[i] = g->c[0];
+			str[i] = g->c;
 		i++;
 	}
 	str[i] = '\0';
-	write(1, str, ft_strlen(str));
 	return(str);
 }
 
@@ -94,29 +95,43 @@ int read_input(FILE *file, t_global *g, char *str)
 	float	y;
 	int		i;
 
-	res = fscanf(file, "%s %f %f %f %s\n", op.letter, &(op.x), &(op.y), &(op.r), op.c);
+	res = fscanf(file, "%c %f %f %f %c\n", &(op.letter), &(op.x), &(op.y), &(op.r), &(op.c)); //backslash n important
+//	printf("letter = %c | x = %f | y = %f | r = %f | c = %c\n", op.letter, op.x, op.y, op.r, op.c); 
 	while (res == 5)
 	{
-		if ((op.letter[0] != 'C' && op.letter[0] != 'c')
-			|| op.r <= 0)
+		if ((op.letter != 'C' && op.letter != 'c') || op.r <= 0)
 			return (1);
 		i = 0;
 		while (i <g->width * g->height + g->height)
 		{
-			x = i % g->height;
-			y = i % g->width;
-			if (in_circle(x, y, &op) == 2)
-				str[i] = op.c[0];
-			if (in_circle(x, y, &op) == 1 && op.letter[0] == 'C')
-				str[i] = op.c[0];
+			if (str[i] != '\n')
+			{
+				x = i % (g->width + 1); // a retenir
+				y = i / g->width; //  a retenir
+		//		printf("i = %d | x = %f | y = %f\n",i, x , y);
+				if (in_circle(x, y, &op) == 2)
+					str[i] = op.c;
+				if (in_circle(x, y, &op) == 1 && op.letter == 'C')
+					str[i] = op.c;
+			}
 			i++;
 		}
-		res = fscanf(file, "%s %f %f %f %s", op.letter, &(op.x), &(op.y), &(op.r), op.c);
+		res = fscanf(file, "%c %f %f %f %c\n", &(op.letter), &(op.x), &(op.y), &(op.r), &(op.c));
 	}
-	if (res != 0)
+	if (res != -1) // - 1 quand fini
 		return(1);
 	return(0);
-	//comment trouver une erreur de lecture?
+}
+
+int ft_clear(FILE *file, char *str, char *error)
+{
+	if (file)
+		fclose(file);
+	if(str)
+		free(str);
+	if(error)
+		ft_error(error);
+	return(1);
 }
 
 int main(int ac, char **av)
@@ -131,19 +146,13 @@ int main(int ac, char **av)
 	file = fopen(av[1], "r");
 	if (file == NULL)//ouvre ou lit le fichier
 		return(ft_error("Error: Operation file corrupted\n"));
-	printf("ok1\n");
 	str = read_first_line(file, &g);
-	printf("ok1\n");
 	if (str == NULL)
-	{
-		fclose(file);
-		return (1);
-	}
-	write(1, str, ft_strlen(str));
+		return(ft_clear(file, NULL, "Error: Operation file corrupted\n")); // quand pbm str
 	res = read_input(file, &g, str); 
-	if (res == 0)
-		write(1, str, ft_strlen(str));
-	free(str);
-	fclose(file);
-	return(res); //tout bon
+	if (res == 1)
+		return(ft_clear(file, str, "Error: Operation file corrupted\n")); // quand pbm input
+	write(1, str, ft_strlen(str));
+	ft_clear(file, str, NULL);
+	return(0); //tout bon
 }
